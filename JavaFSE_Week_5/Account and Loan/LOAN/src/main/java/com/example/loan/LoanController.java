903fc726be3176e4471261affe7228774fe33aa3
sudheeroo7 <sudheersud2005@gmail.com>
2025-07-20 09:45:18 +0530

    Added JavaFSE_Week_5 folder

---
+++ JavaFSE_Week_5/Account and Loan/LOAN/src/main/java/com/example/loan/LoanController.java
@@ -0,0 +1,30 @@
+package com.example.loan;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Loan getLoanDetails(@PathVariable String number) {
+        return new Loan("H00987987972342", "car", 400000, 3258, 18);
+    }
+
+    static class Loan {
+        public String number;
+        public String type;
+        public double loan;
+        public double emi;
+        public int tenure;
+
+        public Loan(String number, String type, double loan, double emi, int tenure) {
+            this.number = number;
+            this.type = type;
+            this.loan = loan;
+            this.emi = emi;
+            this.tenure = tenure;
+        }
+    }
+}
+
